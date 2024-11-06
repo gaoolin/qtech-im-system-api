8622dbf0e8bc1e2c312beabfebf93d60090dad8d
zhilin.gao <gaoolin@gmail.com>
2024-11-06 09:53:49 +0800

    refactor(framework): 重构 ImConfig 类
    - 为 ImConfig 类的字段添加 lombok.Getter 注解
    - 删除冗余的 getter 方法
    - 优化代码结构，提高可读性和维护性

--- src/main/java/com/qtech/framework/config/ImConfig.java
+++ src/main/java/com/qtech/framework/config/ImConfig.java
@@ -1,5 +1,6 @@
 package com.qtech.framework.config;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,33 +14,28 @@ import org.springframework.stereotype.Component;
 public class ImConfig
 {
     /** 项目名称 */
+    @Getter
     private String name;
 
     /** 版本 */
+    @Getter
     private String version;
 
     /** 版权年份 */
+    @Getter
     private String copyrightYear;
 
     /** 上传路径 */
+    @Getter
     private static String profile;
 
     /** 获取地址开关 */
+    @Getter
     private static boolean addressEnabled;
-
-    public String getName()
-    {
-        return name;
-    }
 
     public void setName(String name)
     {
         this.name = name;
-    }
-
-    public String getVersion()
-    {
-        return version;
     }
 
     public void setVersion(String version)
@@ -47,29 +43,14 @@ public class ImConfig
         this.version = version;
     }
 
-    public String getCopyrightYear()
-    {
-        return copyrightYear;
-    }
-
     public void setCopyrightYear(String copyrightYear)
     {
         this.copyrightYear = copyrightYear;
     }
 
-    public static String getProfile()
-    {
-        return profile;
-    }
-
     public void setProfile(String profile)
     {
         ImConfig.profile = profile;
-    }
-
-    public static boolean isAddressEnabled()
-    {
-        return addressEnabled;
     }
 
     public void setAddressEnabled(boolean addressEnabled)
